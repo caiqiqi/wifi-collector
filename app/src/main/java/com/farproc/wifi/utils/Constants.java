911751a81ef9849e9d6878d6c9915685514fb12f
CaiQiqi <564933437@qq.com>
2016-06-06 00:28:31 +0800

    modified Constants.java

--- app/src/main/java/com/farproc/wifi/utils/Constants.java
+++ app/src/main/java/com/farproc/wifi/utils/Constants.java
@@ -5,7 +5,8 @@ public interface Constants {
 	String SERVER_IP = "192.168.23.4";
 	
 	int SERVER_PORT = 30000;
-	
-	String MESSAGE_TO_BE_SENT            = "message to be sent";
-	String MESSAGE_RECEIVED_FROM_SERVER  = "message received from the server";
+
+	//因为这里两个常量是为了给msg.what用的，所以只能是int类型，而不能是String类型
+	int MESSAGE_TO_BE_SENT            = 1;
+	int MESSAGE_RECEIVED_FROM_SERVER  = 2;
 }
